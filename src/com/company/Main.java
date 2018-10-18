9c068acf8f62a4c3f14f9af3d60c4f7d4838f256
fvictoria <felipevictoria630@yahoo.com>
2018-10-17 22:09:23 -0400

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String input, sp[];
+
+        do {
+            System.out.println("Type in a sentence and press ENTER");
+            Scanner kbReader = new Scanner(System.in);
+            input = kbReader.nextLine();
+            input += "z";
+            if (input.equals("EX".toUpperCase()))
+                break;
+
+            sp = input.split("S\\s*A");
+
+            System.out.println("There are " + (sp.length - 1) + " occurrences");
+
+        }
     }
 }
+
+
+
